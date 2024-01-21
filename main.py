ef022c4efda32ceb535c33bfc348eeec7c06b867
Andrew Ursu <aursu@sfu.ca>
2024-01-20 21:16:12 -0800

    got clicking contact info working

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import sys
 from scrape_profile import scrape_profile
-
+import time
 import re
 def login(driver) :
     # Open LinkedIn login page
@@ -32,22 +32,26 @@ def login(driver) :
     )
     
 
-def find_users_with_job(driver) :
+def find_users_with_job(driver, num_profiles_r) :
     driver.get("https://www.google.com")
 
     search_query = driver.find_element(By.NAME, "q")
 
     search_query.send_keys('site:linkedin.com/in/ AND "python developer" AND "London"')
     search_query.send_keys(Keys.RETURN)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "search")))
-    
-    html_content = driver.page_source
+    profile_links = []
+    while len(profile_links) < num_profiles_r :
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "search")))
+        
+        html_content = driver.page_source
 
-    profile_links = find_links(html_content, r'https://\w+\.linkedin\.com/in/\w+')
-    
+        profile_links = find_links(html_content, r'https://\w+\.linkedin\.com/in/\w+', num_profiles_r)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        
     return profile_links
 
-def find_links(html_content, regex_pattern) :
+def find_links(html_content, regex_pattern, num_profiles_r) :
     matches = re.findall(regex_pattern, html_content)
 
     unique_matches = list(set(matches))
@@ -60,8 +64,8 @@ if __name__ == "__main__" :
     driver = webdriver.Chrome()
 
     login(driver)
-    num_profiles = 5
-    profile_links = find_users_with_job(driver)
+    num_profiles_r = 5
+    profile_links = find_users_with_job(driver, num_profiles_r)
 
     scrape_profile(driver, profile_links)
     driver.quit()
--- scrape_profile.py
+++ scrape_profile.py
@@ -1,3 +1,11 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 def scrape_profile(driver, profiles) :
     for profile in profiles :
-        print(profile)
+        print("profile link is", profile)
+        driver.get(profile)
+        wait = WebDriverWait(driver, 10)  # Adjust the timeout as needed
+        element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[text()='Contact info']")))
+        element.click()
